479be715e6bb22f083aee6cc11e978b75a847301
marmin-dev <marmin.dev@gmail.com>
2023-07-05 15:16:45 +0900

    start project

---
+++ src/main/java/com/story/sangboo/SangbooApplication.java
@@ -0,0 +1,13 @@
+package com.story.sangboo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SangbooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SangbooApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/story/sangboo/SangbooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.story.sangboo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SangbooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
