35cc22e4a755985ddf93a0c1047d3893209ed6da
marmin-dev <marmin.dev@gmail.com>
2023-07-12 10:55:53 +0900

    deleting playlist repo

--- src/main/java/com/story/sangboo/repositories/PlaylistRepository.java
+++
@@ -1,7 +0,0 @@
-package com.story.sangboo.repositories;
-
-import com.story.sangboo.entities.Playlist;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PlaylistRepository extends JpaRepository<Playlist,Long> {
-}
